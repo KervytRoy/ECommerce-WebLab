853867bf82d76126463224228df4f2baa577e041
kervyt <kervytroy96@gmail.com>
2024-05-22 12:02:21 -0400

    new product list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'products-page', component: ProductsPageComponent },
 ];
 
 @NgModule({
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,10 @@
+import { SearchProductsRequest } from './../../api/models/search-products-request';
 import { Component } from '@angular/core';
+import { ProductsService } from '../../api/services';
+import { ProductsSearch$Params } from '../../api/fn/products/products-search';
+import { PaginationResponseOfProductDto } from '../../api/models';
+
+
 
 
 @Component({
@@ -7,5 +13,20 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+   constructor(private productService: ProductsService){
+    let searchProductsRequest: SearchProductsRequest = {
+       pageSize: 10,
+       pageNumber: 1,
+       brandId: 'E0D19668-4449-4F20-790F-08DC6FB73E6B'
+    };
+    let params: ProductsSearch$Params = {
+      body: searchProductsRequest
+    };
+    this.productService.productsSearch(params).subscribe(
+      (response: PaginationResponseOfProductDto) => {
+        console.log(response);
+      }
+    );
+   }
+   
 }
